3d2eb008a2bc5c0bd49f663b51e1e1d214c8c3ff
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2024-02-08 11:02:41 -0500

    [REL] Nueva versión liberada 0.5.3

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         ],
-    version='0.5.2',
+    version='0.5.3',
 
     # ######################################################################
     # FILES
