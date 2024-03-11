f47619034b83cfc073681568ab6aa8e2b9c5d285
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2024-03-11 00:28:48 -0500

    [FEAT] Solución del problema de dos cuerpos, plotly en gráficos, cónicas interactivas

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         ],
-    version='0.5.5',
+    version='0.5.10',
 
     # ######################################################################
     # FILES
--- src/pymcel/__init__.py
+++ src/pymcel/__init__.py
@@ -213,7 +213,7 @@ def encuentra_rangos(rs):
             [y_middle - plot_radius, y_middle + plot_radius],
             [z_middle - plot_radius, z_middle + plot_radius])
 
-def fija_ejes3d_proporcionales(ax):
+def fija_ejes3d_proporcionales(ax,rangos=None):
     """Ajusta los ejes en 3d para hacelos proporcionales.
 
     Hace que los ejes de un gráfico en 3d tengan la misma escala, de
@@ -237,17 +237,25 @@ def fija_ejes3d_proporcionales(ax):
     y_limits = ax.get_ylim3d()
     z_limits = ax.get_zlim3d()
 
-    x_range = abs(x_limits[1] - x_limits[0])
-    x_middle = np.mean(x_limits)
-    y_range = abs(y_limits[1] - y_limits[0])
-    y_middle = np.mean(y_limits)
-    z_range = abs(z_limits[1] - z_limits[0])
-    z_middle = np.mean(z_limits)
+    if rangos == None:
+        x_range = abs(x_limits[1] - x_limits[0])
+        x_middle = np.mean(x_limits)
+        y_range = abs(y_limits[1] - y_limits[0])
+        y_middle = np.mean(y_limits)
+        z_range = abs(z_limits[1] - z_limits[0])
+        z_middle = np.mean(z_limits)
+    else:
+        x_range = abs(rangos[0][1] - rangos[0][0])
+        x_middle = (rangos[0][1] + rangos[0][0])/2
+        y_range = abs(rangos[1][1] - rangos[1][0])
+        y_middle = (rangos[1][1] + rangos[1][0])/2
+        z_range = abs(rangos[2][1] - rangos[2][0])
+        z_middle = (rangos[2][1] + rangos[2][0])/2
 
     # The plot bounding box is a sphere in the sense of the infinity
     # norm, hence I call half the max range the plot radius.
-    plot_radius = 0.5*max([x_range, y_range, z_range])
-
+    plot_radius = 0.55*max([x_range, y_range, z_range])
+    
     ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
     ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
     ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
@@ -259,12 +267,16 @@ def plot_ncuerpos_3d(rs,vs,tipo='matplotlib',**opciones):
     #Número de partículas
     N=rs.shape[0]
 
-    if tipo == 'matplotlib':  
+    if tipo == 'matplotlib':
+
+        opciones_defecto = dict(lw=1)
+        opciones_defecto.update(opciones)
+
         fig=plt.figure()
         ax=fig.add_subplot(111,projection='3d')
 
         for i in range(N):
-            ax.plot(rs[i,:,0],rs[i,:,1],rs[i,:,2],**opciones);
+            ax.plot(rs[i,:,0],rs[i,:,1],rs[i,:,2],**opciones_defecto);
 
         fija_ejes3d_proporcionales(ax);
         fig.tight_layout();
@@ -272,6 +284,14 @@ def plot_ncuerpos_3d(rs,vs,tipo='matplotlib',**opciones):
         return fig
 
     elif tipo == 'plotly':
+
+        opciones_defecto = dict(
+            mode='lines',
+            name='Cuerpo',
+            marker=dict(),
+            line=dict(),
+        )
+        opciones_defecto.update(opciones)
 
         try:
             import plotly.graph_objects as go
@@ -287,8 +307,9 @@ def plot_ncuerpos_3d(rs,vs,tipo='matplotlib',**opciones):
             fig.add_trace(
                 go.Scatter3d(
                     x=xs, y=ys, z=zs,
-                    mode='lines',
-                    name=f"Cuerpo {i}"
+                    mode=opciones_defecto['mode'],
+                    name=opciones_defecto['name']+f" {i}",
+                    marker=opciones_defecto['marker'],
                 )
             )
         rmin = rs.min()
@@ -304,24 +325,46 @@ def plot_ncuerpos_3d(rs,vs,tipo='matplotlib',**opciones):
 
     return fig
 
-def plot_doscuerpos_3d(rs,vs,tipo='matplotlib',**opciones):
+from scipy.interpolate import interp1d
+def plot_doscuerpos_3d(rs,vs,tipo='matplotlib',ts=None,**opciones):
 
     #Número de partículas
     N=rs.shape[0]
 
+    if ts is not None:
+        xfun = interp1d(ts,rs[:,0],kind='cubic')
+        yfun = interp1d(ts,rs[:,1],kind='cubic')
+        zfun = interp1d(ts,rs[:,2],kind='cubic')
+        tss = np.linspace(ts[0],ts[-1],10*len(ts))
+        rs = np.array([[xfun(t),yfun(t),zfun(t)] for t in tss])
+        print(rs.shape)
+        
     if tipo == 'matplotlib':  
+    
+        opciones_defecto = dict(color='k',lw=1)
+        opciones_defecto.update(opciones)
+    
         fig=plt.figure()
         ax=fig.add_subplot(111,projection='3d')
 
-        for i in range(N):
-            ax.plot(rs[:,0],rs[:,1],rs[:,2],**opciones);
+        ax.plot(rs[:,0],rs[:,1],rs[:,2],**opciones_defecto);
 
-        fija_ejes3d_proporcionales(ax);
+        rangos = encuentra_rangos(rs)
+        fija_ejes3d_proporcionales(ax,rangos);
+
         fig.tight_layout();
         plt.show();
         return fig
 
     elif tipo == 'plotly':
+        
+        opciones_defecto = dict(
+            mode='lines',
+            name='Vector relativo',
+            marker=dict(color='Black'),
+            line=dict(color='Black'),
+        )
+        opciones_defecto.update(opciones)
 
         try:
             import plotly.graph_objects as go
@@ -330,17 +373,17 @@ def plot_doscuerpos_3d(rs,vs,tipo='matplotlib',**opciones):
             return None
 
         fig = go.Figure()
-        for i in range(N):
-            xs = rs[:,0]
-            ys = rs[:,1]
-            zs = rs[:,2]
-            fig.add_trace(
-                go.Scatter3d(
-                    x=xs, y=ys, z=zs,
-                    mode='lines',
-                    name=f"Cuerpo {i}"
-                )
+        xs = rs[:,0]
+        ys = rs[:,1]
+        zs = rs[:,2]
+        fig.add_trace(
+            go.Scatter3d(
+                x=xs, y=ys, z=zs,
+                mode=opciones_defecto['mode'],
+                name=opciones_defecto['name'],
+                marker=opciones_defecto['marker'],
             )
+        )
         rmin = rs.min()
         rmax = rs.max()
 
@@ -647,8 +690,7 @@ def edm_dos_cuerpos(Y,t,mu):
 
 def doscuerpos_solucion(mu,r,v,ts):
     X0 = np.concatenate([r,v])
-    ts = np.linspace(0,10,300)
-    solucion = odeint(edm_dos_cuerpos,X0,ts,args=(1,))
+    solucion = odeint(edm_dos_cuerpos,X0,ts,args=(mu,))
     rs = solucion[:,:3]
     vs = solucion[:,3:]
     return rs,vs
@@ -1280,3 +1322,104 @@ def kepler_bessel(M,e,delta):
 def serie_stumpff(t,k,N=15):
     sk=lambda n:t/((2*n+k+1)*(2*n+k+2))*(1-sk(n+1)) if n<N else 0
     return (1-sk(0))/math.factorial(k)
+
+def plot_elipse(e=0.5,a=10.0):
+
+    e=float(e)
+    a=float(a)
+    if e>1:
+        raise ValueError("La excentricidad de una elipse debe ser menor que 1")
+    if a<0:
+        raise ValueError("El semieje mayor de una elipse debe ser positivo")
+
+    b=a*sqrt(1-e**2)
+
+    #Distancia foco-centro
+    c=a*e
+
+    #Máximo valor de x
+    xcmax=a
+
+    #Valores de x en los que graficaremos
+    xcs=linspace(-a,a,100)
+
+    #Ecuaciones de las cónicas referidas al apside
+    ycs_cir=a*sqrt(1-xcs**2/a**2)
+    ycs=b*sqrt(1-xcs**2/a**2)
+
+    #Gráfica
+    import matplotlib.pyplot as plt
+    fig=plt.figure(figsize=(6,6))
+    ax=fig.gca()
+
+    ax.plot(xcs,ycs_cir,'k--')
+    ax.plot(xcs,-ycs_cir,'k--')
+    ax.plot(xcs,ycs,'r')
+    ax.plot(xcs,-ycs,'r')
+
+    #Graficar la posición del foco y el centro
+    ax.plot([-c],[0],'bx',markersize=10)
+    ax.plot([0],[0],'ko',markersize=5)
+
+    #Decoración
+    ax.grid()
+    ax.set_title(f"Elipse con $a = {a}$, $e={e}$")
+
+    #Fijamos la misma escala en los ejes
+    plt.axis("equal")
+    plt.show()
+
+#Definimos el algoritmo como una rutina
+def plot_hiperbola(e=1.5,a=-10):
+
+    e=float(e)
+    a=float(a)
+
+    if e<1:
+        raise ValueError("La excentricidad de una hipérbola debe ser mayor que 1")
+    if a>0:
+        raise ValueError("El semieje mayor de una hipérbola debe ser negativo")
+
+    #Semieje menor
+    beta=abs(a)*sqrt(e**2-1)
+
+    #Semilatus rectum
+    p=a*(1-e**2)
+
+    #Posición del foco
+    q=p/(1+e)
+    F=abs(a)+q
+
+    #Máximo valor de x
+    xcmax=3*abs(a)
+
+    #Valores de x en los que graficaremos
+    xcs=linspace(abs(a),xcmax,100)
+
+    #Ecuaciones de las cónicas referidas al apside
+    ycs=beta*sqrt(xcs**2/a**2-1)
+
+    #Ecuación de las asintotas
+    xas=linspace(0,xcmax,100)
+    ycs_asi=beta*xas/abs(a)
+
+    #Gráfica
+    fig=plt.figure(figsize=(6,6))
+    ax=fig.gca()
+
+    ax.plot(xas,ycs_asi,'k--')
+    ax.plot(xas,-ycs_asi,'k--')
+    ax.plot(xcs,ycs,'r')
+    ax.plot(xcs,-ycs,'r')
+
+    #Graficar la posición del foco y el vértice
+    ax.plot([F],[0],'bx',markersize=10)
+    ax.plot([0],[0],'ko',markersize=5)
+
+    #Decoración
+    ax.grid()
+    ax.set_title(f"Hipérbola con $a = {a}$, $e={e}$")
+
+    #Fijamos la misma escala en los ejes
+    plt.axis("equal")
+    plt.show()
