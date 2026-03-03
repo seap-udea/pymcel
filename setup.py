46f302bd305b2fb0a937359e0eb34af2eda43d17
Web Developer <jorge.zuluaga@udea.edu.co>
2026-03-03 08:15:15 -0500

    [MAN] Maintenance [setup.py,src/pymcel/version.py]

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         ],
-    version='0.9.14',
+    version='0.9.15',
 
     # ######################################################################
     # FILES
